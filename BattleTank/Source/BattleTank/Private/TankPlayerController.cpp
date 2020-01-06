efea61c456862e3f1578cbc01df699bb21bfc0a0
ManChunFu <man.chunfu@futuregames.nu>
2020-01-06 21:17:18 +0100

    create Tank and TankPlayerController c++ to connect to BP

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,3 +1,4 @@
+
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
@@ -6,4 +7,17 @@
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{ UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank.")); }
+	else
+	{ UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *(ControlledTank->GetName())); }
+
 }
